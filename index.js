46d62b1b9fee244c9384f22504aac015a5874f89
Marvin Alegre <alegremrvn@protonmail.com>
2023-06-01 18:35:34 +0800

    Another commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const PORT = 3000
 
@@ -33,7 +35,8 @@ const tests = {
           "Res. No. 2",
           "Res. No. 1378",
           "Res. No. 3",
-        ]
+        ],
+        answer: 0
       },
     ]
   },
@@ -57,6 +60,7 @@ const tests = {
 //////////////////////////////////////////////
 
 app.use('/dist', express.static(__dirname + '/dist'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 //////////////////////////////////////////////
 //////////////////////////////////////////////
@@ -70,22 +74,111 @@ app.get('/api/tests', (req, res) => {
   res.json(result)
 })
 
-app.get('/addtest', (req, res) => {
-  res.sendFile(__dirname + '/views/addTest.html')
+app.get('/createtest', (req, res) => {
+  res.sendFile(__dirname + '/views/create-test.html')
 })
 
-app.post('/addtest', (req, res) => {
+app.post('/createtest', (req, res) => {
+  tests[req.body.testName] = {
+    nameLc: req.body.testName
+      .replace(/\s/g, "")
+      .toLowerCase(),
+    items: []
+  }
 
-  res.sendFile(__dirname + '/views/test.html')
+  res.redirect(`/test/${tests[req.body.testName].nameLc}`)
 })
 
-// app.get('/plumbingcode', (req, res) => {
-//   res.sendFile(__dirname + '/views/test.html')
-// })
+app.get('/createitem/:testName', (req, res) => {
+  res.sendFile(__dirname + '/views/create-item.html')
+})
 
-// app.get('/plumbingcodeitems', (req, res) => {
-//   res.json(testItems['Plumbing Code'])
-// })
+app.post('/createitem', (req, res) => {
+  let arr = req.headers.referer.split('/')
+  let testName = arr[arr.length - 1]
+  for (let test in tests) {
+    if (tests[test].nameLc === testName) {
+      let answerBaseZero
+      switch (req.body.answer) {
+        case 'a':
+          answerBaseZero = 0
+          break
+        case 'b':
+          answerBaseZero = 1
+          break
+        case 'c':
+          answerBaseZero = 2
+          break
+        case 'd':
+          answerBaseZero = 3
+          break
+      }
+
+      tests[test].items.push({
+        "question": req.body.question,
+        options: [
+          req.body.optA,
+          req.body.optB,
+          req.body.optC,
+          req.body.optD,
+        ],
+        answer: answerBaseZero
+      })
+
+      res.redirect('/createitem/' + testName)
+    }
+  }
+
+})
+
+app.get('/updateitem/:testName/:itemNumber', (req, res) => {
+  res.sendFile(__dirname + '/views/update-item.html')
+})
+
+app.post('/updateitem/:testName/:itemNumber', (req, res) => {
+  for (let test in tests) {
+    if (tests[test].nameLc === req.params.testName) {
+      let item = tests[test].items[req.params.itemNumber - 1]
+      item.question = req.body.question
+      item.options = []
+      item.options.push(req.body.optA)
+      item.options.push(req.body.optB)
+      item.options.push(req.body.optC)
+      item.options.push(req.body.optD)
+
+      let answer
+      switch (req.body.answer) {
+        case 'a':
+          answer = 0
+          break
+        case 'b':
+          answer = 1
+          break
+        case 'c':
+          answer = 2
+          break
+        case 'd':
+          answer = 3
+          break
+      }
+      item.answer = answer
+
+      break
+    }
+  }
+
+  res.redirect(`/test/${req.params.testName}`)
+})
+
+app.get('/deleteitem/:testName/:itemNumber', (req, res) => {
+  for (let test in tests) {
+    if (tests[test].nameLc === req.params.testName) {
+      tests[test].items.splice(req.params.itemNumber - 1, 1)
+    }
+  }
+
+  res.redirect('/test/' + req.params.testName)
+})
 
 app.get('/test/:testName', (req, res) => {
   res.sendFile(__dirname + '/views/test.html')
